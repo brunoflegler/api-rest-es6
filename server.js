23c063f15b3c89f0913c4d3d33a90c11d9734045
Bruno Flegler DalCol <bruno.dalcol@el.com.br>
2018-06-23 21:58:19 -0300

    server port

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ DBConnectMongoose().then(() => {
 	//configure routes
 	routes.assign(app)
 
-	app.listen(3000)
+	const port  = process.env.PORT || 3000
+	app.listen(port)
 	console.log('Server listening on port 3000')
 
 }).catch((err) => {
